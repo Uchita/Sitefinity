7095a63b2cc0b8c80179f7c72abfc75af0ff77cc
Liam <Liam@jxt.com.au>
2016-10-28 10:54:52 +1100

    Updated EmailSender namespace to JXTPortal.EmailSender

--- EmailSender/MessagePriority.cs
+++ EmailSender/MessagePriority.cs
@@ -1,4 +1,4 @@
-namespace EmailSender
+namespace JXTPortal.EmailSender
 {
     /// <summary>
     /// Message priority
--- Services/JXTJobAlerts/Common/Utils.cs
+++ Services/JXTJobAlerts/Common/Utils.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using EmailSender;
+using JXTPortal.EmailSender;
 using System.Configuration;
 using System.Net.Configuration;
 using System.Security.Cryptography;

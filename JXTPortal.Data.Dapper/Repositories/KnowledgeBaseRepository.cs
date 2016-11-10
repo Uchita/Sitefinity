554349cae9a3e27c6963b311c9ecc788d95c0973
Liam <Liam@jxt.com.au>
2016-11-10 16:57:15 +1100

    Implemented concrete BaseEntityOperation<T> to reduce code duplication for Repositories

--- JXTPortal.Data.Dapper/Repositories/KnowledgeBaseRepository.cs
+++ JXTPortal.Data.Dapper/Repositories/KnowledgeBaseRepository.cs
@@ -13,75 +13,13 @@ namespace JXTPortal.Data.Dapper.Repositories
     {
     }
 
-    public class KnowledgeBaseRepository : IKnowledgeBaseRepository
+    public class KnowledgeBaseRepository : BaseEntityOperation<KnowledgeBaseEntity>, IKnowledgeBaseRepository
     {
-        private readonly IConnectionFactory _connectionFactory;
-
-        private readonly string _connectionStringName;
-
         public KnowledgeBaseRepository(IConnectionFactory connectionFactory, string connectionStringName)
+            :base(connectionFactory, connectionStringName)
         {
-            this._connectionFactory = connectionFactory;
-            _connectionStringName = connectionStringName;
+            TableName = "KnowledgeBase";
+            ColumnNames = new List<string>{"KnowledgeBaseCategoryID", "Subject", "Content", "Valid", "Sequence", "LastModified", "LastModifiedBy", "SearchField", "Tags"};
         }
-
-        #region IBaseEntityOperation<KnowledgeBaseRepository> Members
-
-        public int Insert(KnowledgeBaseEntity entity)
-        {
-            using (IDbConnection dbConnection = _connectionFactory.Create(_connectionStringName))
-            {
-                var sbQuery = new StringBuilder();
-                sbQuery.Append("INSERT INTO dbo.KnowledgeBase(KnowledgeBaseCategoryID, Subject, Content, Valid, Sequence, LastModified, LastModifiedBy, SearchField, Tags)");
-                sbQuery.Append(" VALUES(@KnowledgeBaseCategoryID, @Subject, @Content, @Valid, @Sequence, @LastModified, @LastModifiedBy, @SearchField, @Tags)");
-                sbQuery.Append("SELECT CAST(SCOPE_IDENTITY() as int)");
-                var Id = dbConnection.Query<int>(sbQuery.ToString(), entity).Single();
-                return Id;
-            }
-        }
-
-        public void Update(KnowledgeBaseEntity entity)
-        {
-            using (IDbConnection dbConnection = _connectionFactory.Create(_connectionStringName))
-            {
-                dbConnection.Open();
-                var query = "UPDATE dbo.KnowledgeBase SET KnowledgeBaseCategoryID = @KnowledgeBaseCategoryID, Subject = @Subject, Content = @Content, Valid = @Valid, Sequence = @Sequence, LastModified = @LastModified, LastModifiedBy = @LastModifiedBy, SearchField = @SearchField, Tags = @Tags WHERE Id = @Id";
-                dbConnection.Execute(query, entity);
-            }
-        }
-
-        public void Delete(int id)
-        {
-            using (IDbConnection dbConnection = _connectionFactory.Create(_connectionStringName))
-            {
-                dbConnection.Open();
-                var query = "DELETE FROM dbo.KnowledgeBase WHERE Id = @Id";
-                dbConnection.Execute(query, new { Id = id });
-            }
-        }
-
-        public KnowledgeBaseEntity Select(int id)
-        {
-            using (IDbConnection dbConnection = _connectionFactory.Create(_connectionStringName))
-            {
-                dbConnection.Open();
-                var query = "SELECT Id, KnowledgeBaseCategoryID, Subject, Content, Valid, Sequence,LastModified, LastModifiedBy, SearchField, Tags FROM dbo.KnowledgeBase WHERE Id = @Id";
-                var entity = dbConnection.Query<KnowledgeBaseEntity>(query, new { Id = id }).SingleOrDefault();
-                return entity;
-            }
-        }
-
-        public List<KnowledgeBaseEntity> SelectAll()
-        {
-            using (IDbConnection dbConnection = _connectionFactory.Create(_connectionStringName))
-            {
-                dbConnection.Open();
-                var query = "SELECT Id, KnowledgeBaseCategoryID, Subject, Content, Valid, Sequence,LastModified, LastModifiedBy, SearchField, Tags FROM dbo.KnowledgeBase";
-                var entities = dbConnection.Query<KnowledgeBaseEntity>(query).ToList();
-                return entities;
-            }
-        }
-
-        #endregion
     }
 }

0d4c22249c22a3610591dff434b339f4fddd391f
danieljxt <daniel.ng@jxt.com.au>
2016-12-08 09:54:02 +1100

    Member Activity Changes

---
+++ JXTPortal.Website/Admin/reports/MemberActivity.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/admin.Master" AutoEventWireup="true"
+    CodeBehind="MemberActivity.aspx.cs" Inherits="JXTPortal.Website.Admin.reports.MemberActivity" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder3" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    Member Activity
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="form-all">
+        <asp:Panel ID="pnlSite" runat="server">
+            <ul class="form-section">
+                <li class="form-line">
+                    <label class="form-label-left">
+                        Site:&nbsp;
+                    </label>
+                    <div class="form-input-wide">
+                        <asp:DropDownList ID="ddlSite" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlSite_SelectedIndexChanged" />
+                    </div>
+                </li>
+            </ul>
+        </asp:Panel>
+        <asp:Repeater ID="rptMemberActivity" runat="server" 
+            onitemdatabound="rptMemberActivity_ItemDataBound1">
+            <HeaderTemplate>
+                <ul class="form-section">
+            </HeaderTemplate>
+            <ItemTemplate>
+                <li class="form-line">
+                    <label class="form-label-left">
+                        <strong>
+                            <asp:Literal ID="litTitle" runat="server" />:&nbsp;</strong>
+                    </label>
+                    <div class="form-input-wide">
+                        <asp:Literal ID="litTotal" runat="server" />
+                    </div>
+                </li>
+            </ItemTemplate>
+            <FooterTemplate>
+                </ul>
+            </FooterTemplate>
+        </asp:Repeater>
+    </div>
+</asp:Content>
---
+++ JXTPortal.Website/Admin/reports/MemberActivity.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using JXTPortal.Entities;
+using JXTPortal;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JXTPortal.Website.Admin.reports
+{
+    public partial class MemberActivity : System.Web.UI.Page
+    {
+        #region Declarations
+        MembersService _membersService;
+        SitesService _sitesService;
+        #endregion
+
+        #region Properties
+
+        private int _siteID
+        {
+            get
+            {
+                if (SessionData.AdminUser != null && SessionData.AdminUser.isAdminUser == false)
+                {
+                    return Convert.ToInt32(SessionData.Site.SiteId);
+                }
+
+                if (ddlSite.SelectedItem != null && ddlSite.SelectedValue.Length > 0 && Convert.ToInt32(ddlSite.SelectedValue) > 0)
+                {
+                    return Convert.ToInt32(ddlSite.SelectedValue);
+                }
+                return 0;
+            }
+        }
+
+        MembersService MembersService
+        {
+            get
+            {
+                if (_membersService == null)
+                {
+                    _membersService = new MembersService();
+                }
+                return _membersService;
+            }
+        }
+
+        SitesService SitesService
+        {
+            get
+            {
+                if (_sitesService == null)
+                {
+                    _sitesService = new SitesService();
+                }
+                return _sitesService;
+            }
+        }
+        #endregion
+
+        #region Page
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                LoadSite();
+            }
+
+            if (SessionData.AdminUser != null && SessionData.AdminUser.AdminRoleId != 1)
+                pnlSite.Visible = false;
+
+            LoadMemberActivity();
+        }
+        #endregion
+
+        #region Methods
+        private void LoadSite()
+        {
+            List<JXTPortal.Entities.Sites> sites = new List<JXTPortal.Entities.Sites>();
+
+            if (SessionData.AdminUser != null && SessionData.AdminUser.isAdminUser == true)
+            {
+                sites = SitesService.GetAll().OrderBy(s => s.SiteName).ToList();
+            }
+            else
+            {
+                sites.Add(SitesService.GetBySiteId(SessionData.Site.SiteId));
+                pnlSite.Visible = false;
+            }
+
+            ddlSite.DataSource = sites;
+            ddlSite.DataTextField = "SiteName";
+            ddlSite.DataValueField = "SiteID";
+            ddlSite.DataBind();
+
+            ddlSite.Items.Insert(0, new ListItem("-All-", "0"));
+            ddlSite.SelectedValue = SessionData.Site.SiteId.ToString();
+        }
+
+        private void LoadMemberActivity()
+        {
+            int sitePageCount = JXTPortal.Common.Utils.GetAppSettingsInt("SitePaging");
+            int totalCount = 0;
+
+            TList<Entities.Members> members = MembersService.GetPaged("SiteID = " + _siteID.ToString(), "LastModifiedDate DESC", 0, sitePageCount, out totalCount);
+            rptMemberActivity.DataSource = members;
+            rptMemberActivity.DataBind();
+        }
+        #endregion
+
+        #region Events
+        protected void rptMemberActivity_ItemDataBound1(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+
+            }
+        }
+        #endregion
+
+        #region Events
+
+        protected void ddlSite_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadMemberActivity();
+        }
+
+        #endregion
+
+    }
+}

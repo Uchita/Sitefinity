71ddb711fa1a15d549b08a7b5d03dddd4d0e538b
Liam <Liam@jxt.com.au>
2016-12-14 16:34:34 +1100

    Updated JobScienceIntegration to utilise Log4net instead of ExceptionTable

--- JXTJobScienceIntegration/Program.cs
+++ JXTJobScienceIntegration/Program.cs
@@ -16,6 +16,7 @@ using EmailSender;
 using JXTPortal.Entities;
 using System.Web;
 using System.Web.Script.Serialization;
+using log4net;
 
 namespace JXTJobScienceIntegration
 {
@@ -467,17 +468,10 @@ namespace JXTJobScienceIntegration
                     }
                     catch (Exception ex)
                     {
-                        int exceptionID = LogExceptionAndEmail(siteXML, strApplicationID, ex);
-
-                        Console.WriteLine("ERROR: (" + exceptionID + ") " + ex.Message);
-
-
+                        ILog logger = LogManager.GetLogger(typeof(Program));
+                        logger.Error(ex);
                     }
                 }
-
-
-
-
             }
 
         }
@@ -501,13 +495,8 @@ namespace JXTJobScienceIntegration
             catch (Exception ex)
             {
                 continueToNextApplication = false;
-                int exceptionID = LogExceptionAndEmail(siteXML, JobApplicationID, ex);
-                Console.WriteLine("[" + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString() + "] ERROR: (" + exceptionID + ") " + ex.Message);
-
-            }
-            finally
-            {
-
+                ILog logger = LogManager.GetLogger(typeof(Program));
+                logger.Error(ex);
             }
 
             return continueToNextApplication;
@@ -526,87 +515,6 @@ namespace JXTJobScienceIntegration
 
             xmlFile.Save(ConfigurationManager.AppSettings["SitesXML"]);
         }
-
-        #region Utils
-
-        /// <summary>
-        /// Email Sender
-        /// </summary>
-        /// <returns></returns>
-        private static SmtpSender EmailSender()
-        {
-            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            MailSettingsSectionGroup mailConfiguration = (MailSettingsSectionGroup)config.GetSectionGroup("system.net/mailSettings");
-
-            SmtpSender mailObject = new SmtpSender(mailConfiguration.Smtp.Network.Host);
-
-            mailObject.Port = mailConfiguration.Smtp.Network.Port;
-            if (!mailConfiguration.Smtp.Network.DefaultCredentials)
-            {
-                mailObject.UserName = mailConfiguration.Smtp.Network.UserName;
-                mailObject.Password = mailConfiguration.Smtp.Network.Password;
-            }
-
-            return mailObject;
-        }
-
-        /// <summary>
-        /// Log the Exception and Send an email.
-        /// </summary>
-        /// <param name="siteXML"></param>
-        /// <param name="strLastExceptionApplicationID"></param>
-        /// <param name="ex"></param>
-        /// <returns></returns>
-        protected static int LogExceptionAndEmail(SitesXML siteXML, string strLastExceptionApplicationID, Exception ex)
-        {
-            ExceptionTableService serviceException = new ExceptionTableService();
-
-            int intExceptionID = serviceException.LogException(ex.GetBaseException());
-
-            XDocument xmlFile = XDocument.Load(ConfigurationManager.AppSettings["SitesXML"]);
-            var query = from c in xmlFile.Elements("sites").Elements("site")
-                        select c;
-            foreach (XElement site in query)
-            {
-                // Save the Exception ID and the application which has exception in the XML.
-                if (site.Element("SiteId").Value == siteXML.SiteId.ToString())
-                {
-                    site.Element("ExceptionID").Value = intExceptionID.ToString();
-                    site.Element("LastExceptionApplicationID").Value = strLastExceptionApplicationID;
-                }
-            }
-
-            xmlFile.Save(ConfigurationManager.AppSettings["SitesXML"]);
-
-
-            // **** Send email when there is an error.
-            Message message = new Message();
-            message.Format = Format.Html;
-
-            message.Body = string.Format(@"
-SiteId: {0}<br /><br />
-ApplicationID: {1}<br /><br />
-DateTime: {2}<br /><br />
-Message: {3}<br /><br />
-StackTrace: {4}<br /><br />
-ExceptionID: {5}",
-                    siteXML.SiteId,
-                    strLastExceptionApplicationID,
-                    DateTime.Now,
-                    ex.Message,
-                    ex.StackTrace,
-                    intExceptionID);
-
-            message.From = new MailAddress("bugs@jxt.com.au", "MiniJXT Support");
-            message.To = new MailAddress(ConfigurationManager.AppSettings["AdminEmail"]);
-            message.Subject = "MiniJXT - Job application FTP Error";
-
-            EmailSender().Send(message);
-
-            return intExceptionID;
-        }
-
-        #endregion
     }
 
     #region Classes

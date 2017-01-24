dbfcb6e7e8ea652db38306ec3550eb4f490a2683
Liam <Liam@jxt.com.au>
2017-01-24 16:15:45 +1100

    Creating a DateExtension Class to add timestamping versions to images

---
+++ JXTPortal.Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JXTPortal.Common.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static double ToEpocTimestamp(this DateTime date)
+        {
+            DateTime epoc = new DateTime(1970, 1, 1, 0, 0, 0);
+
+            var diff = date - epoc;
+
+            return diff.TotalMilliseconds;      
+        }
+
+        public static double? ToEpocTimestamp(this DateTime? date)
+        {
+            if (!date.HasValue)
+            {
+                return null;
+            }
+
+            DateTime epoc = new DateTime(1970, 1, 1, 0, 0, 0);
+
+            var diff = date.Value - epoc;
+
+            return diff.TotalMilliseconds;
+        }
+    }
+}

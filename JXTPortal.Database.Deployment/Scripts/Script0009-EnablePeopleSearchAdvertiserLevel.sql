4bbe4a6d686b83f7481037c2c1edf43dfeef7c64
Himmy Ko <himmy@jxt.com.au>
2017-01-27 15:59:45 +1100

    SQL

---
+++ JXTPortal.Database.Deployment/Scripts/Script0009-EnablePeopleSearchAdvertiserLevel.sql
@@ -0,0 +1,14 @@
+ALTER TABLE Advertisers
+ADD AllowPeopleSearchAccess BIT NOT NULL DEFAULT(0)
+GO
+
+DECLARE @tempTable table (siteID int)
+INSERT INTO @tempTable
+SELECT SiteID FROM GlobalSettings WHERE EnablePeopleSearch = 1
+
+SELECT * FROM @tempTable
+
+UPDATE Advertisers
+SET AllowPeopleSearchAccess = 1
+WHERE SiteID in (SELECT siteID FROM @tempTable)
+

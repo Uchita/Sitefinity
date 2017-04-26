d1b8bd5c4290e6910d0865a0b54470f639528529
Liam_JXT <liam@jxt.com.au>
2017-04-26 16:46:22 +1000

    added if statement to admin/Customscript.js as per Suraj's bug fix

--- JXTPortal.Website/Admin/scripts/customscript.js
+++ JXTPortal.Website/Admin/scripts/customscript.js
@@ -1,11 +1,13 @@
 ﻿$(document).ready(function () {
 
     var dateformat = $("#ctl00_ContentPlaceHolder1_ucJobFields_txtJobExpiryDate").attr('placeholder');
+    if ($("#ctl00_ContentPlaceHolder1_ucJobFields_txtJobExpiryDate").length) {
+        $("#ctl00_ContentPlaceHolder1_ucJobFields_txtJobExpiryDate").datepicker(
+            {
+                minDate: 0,
+                dateFormat: dateformat,
+                maxDate: "+30D"
+            });
+    }
 
-    $("#ctl00_ContentPlaceHolder1_ucJobFields_txtJobExpiryDate").datepicker(
-        {
-            minDate: 0,
-            dateFormat: dateformat,
-            maxDate: "+30D"
-        });
 });

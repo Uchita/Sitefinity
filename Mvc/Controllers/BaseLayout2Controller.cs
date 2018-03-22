d6af5a7b862d9ab7970e0be2a9cab218e38c0f90
Himmy <himmy@jxt.com.au>
2018-03-23 10:27:57 +1100

    source control clean up

---
+++ Mvc/Controllers/BaseLayout2Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "MvcLayout2", Title = "MvcLayout2", SectionName = "MvcLayouts2")]
+    public class BaseLayout2Controller : Controller
+    {
+        public ActionResult MvcLayout2()
+        {
+            return View("MvcLayout2");
+        }
+
+    }
+}

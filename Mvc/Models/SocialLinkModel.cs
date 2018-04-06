dfa4a5b99cb777d2e3a24400d37696872ff96a82
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-04-06 12:05:23 +1000

    Social Link widget initial check-in

---
+++ Mvc/Models/SocialLinkModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitefinityWebApp.Mvc.Models
+{
+    public class SocialLinkModel
+    {
+        public bool IsSelected { get; set; }
+        public string DisplayName { get; set; }
+        public string UrlPath { get; set; }
+        public string EMCssClass { get; set; }
+    }
+}

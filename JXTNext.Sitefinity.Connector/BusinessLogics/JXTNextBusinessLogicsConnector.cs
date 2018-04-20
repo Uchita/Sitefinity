a26fb7601b4a3b10922486095192a046764ca25f
Himmy <himmy@jxt.com.au>
2018-04-20 16:08:53 +1000

    Updated connector to use the latest jxt common api library Updated connectors to process the returned responses from API calls

--- JXTNext.Sitefinity.Connector/BusinessLogics/JXTNextBusinessLogicsConnector.cs
+++ JXTNext.Sitefinity.Connector/BusinessLogics/JXTNextBusinessLogicsConnector.cs
@@ -2,46 +2,68 @@
 using JXTNext.Sitefinity.Connector.BusinessLogics.Mappers;
 using JXTNext.Sitefinity.Connector.BusinessLogics.Models.Advertisers;
 using JXTNext.Sitefinity.Connector.BusinessLogics.Models.Search;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 
 namespace JXTNext.Sitefinity.Connector.BusinessLogics
 {
     public class JXTNextBusinessLogicsConnector : IBusinessLogicsConnector
     {
         string API_TARGET_PATH = ConfigurationManager.AppSettings["JXTNextAPI_Path"];
+        int API_Operation_MaxWaitTimeMs;
         IJobListingMapper _jobMapper;
 
         public JXTNextBusinessLogicsConnector(IJobListingMapper jobMapper)
         {
             _jobMapper = jobMapper;
+
+            bool parseWaitTimeSuccess = int.TryParse(ConfigurationManager.AppSettings["JXTNextAPI_WaitTimeMs"], out API_Operation_MaxWaitTimeMs);
+            if (!parseWaitTimeSuccess)
+                API_Operation_MaxWaitTimeMs = 10000; //set default
         }
 
         public ICreateJobListingResponse AdvertiserCreateJob(ICreateJobListing jobDetails)
         {
             JXTNext_CreateJobListing jobRequest = jobDetails as JXTNext_CreateJobListing;
 
-            ConnectorPostRequest connectorRequest = new ConnectorPostRequest
+            ConnectorPostRequest connectorRequest = new ConnectorPostRequest(API_Operation_MaxWaitTimeMs)
             {
                 Data = _jobMapper.To(jobDetails.JobData),
                 TargetUri = new Uri(API_TARGET_PATH + $"/api/advertiser/job")
             };
             ConnectorResponse response = JXTNext.Common.JXTAPI.Connector.Post(connectorRequest);
 
-            return new JXTNext_CreateJobListingResponse { Success = response.Success, Message = response.Response };
+            //parse the response
+            bool actionSuccessful = response.Success;
+            dynamic data = JObject.Parse(response.Response);
+
+            if( actionSuccessful)
+                return new JXTNext_CreateJobListingResponse { Success = actionSuccessful, Message = response.Response };
+            else
+                return new JXTNext_CreateJobListingResponse { Success = actionSuccessful, Message = ((List<string>)data["errors"]).FirstOrDefault()};
         }
 
         public IGetJobListingResponse AdvertiserGetJob(IGetJobListing jobDetails)
         {
             JXTNext_GetJobListing jobRequest = jobDetails as JXTNext_GetJobListing;
 
-            ConnectorGetRequest connectorRequest = new ConnectorGetRequest
+            ConnectorGetRequest connectorRequest = new ConnectorGetRequest(API_Operation_MaxWaitTimeMs)
             {
                 TargetUri = new Uri(API_TARGET_PATH + $"/api/advertiser/job/{jobRequest.JobID}")
             };
             ConnectorResponse response = JXTNext.Common.JXTAPI.Connector.Get(connectorRequest);
 
-            return new JXTNext_GetJobListingResponse { Success = response.Success, Message = response.Response };
+            //parse the response
+            bool actionSuccessful = response.Success;
+            dynamic data = JObject.Parse(response.Response);
+
+            if (actionSuccessful)
+                return new JXTNext_GetJobListingResponse { Success = actionSuccessful, Message = response.Response };
+            else
+                return new JXTNext_GetJobListingResponse { Success = actionSuccessful, Message = ((List<string>)data["errors"]).FirstOrDefault() };
         }
 
         public void AdvertiserRegister()

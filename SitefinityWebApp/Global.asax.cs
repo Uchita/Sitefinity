c086827bbb4b207f652b4aa51b6dde02d372b89d
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-05-10 18:14:24 +1000

    Job Search test mock connector

--- SitefinityWebApp/Global.asax.cs
+++ SitefinityWebApp/Global.asax.cs
@@ -62,30 +62,6 @@ namespace SitefinityWebApp
             var factory = ObjectFactory.Resolve<ISitefinityControllerFactory>();
             ControllerBuilder.Current.SetControllerFactory(factory);
 
-
-
-
-            //IEnumerable<IBusinessLogicsConnector> _businessLogicsConnectors;
-            //IEnumerable<IOptionsConnector> _optionsConnectors;
-
-            //Get the current lifetime scope from Autofac container
-            //using (var scope = container.BeginLifetimeScope())
-            //{
-            //    _businessLogicsConnectors = scope.Resolve<IEnumerable<IBusinessLogicsConnector>>();
-            //    _optionsConnectors = scope.Resolve<IEnumerable<IOptionsConnector>>();
-            //}
-
-            ////Find the connectors we wawnt
-            //IBusinessLogicsConnector testBusinessLogicConnector = _businessLogicsConnectors.Where(c => c.ConnectorType == JXTNext.Sitefinity.Connector.IntegrationConnectorType.Test).FirstOrDefault();
-            //IOptionsConnector testConnector = _optionsConnectors.Where(c => c.ConnectorType == JXTNext.Sitefinity.Connector.IntegrationConnectorType.Test).FirstOrDefault();
-
-            ////Execute - Get available filter options from the server
-            //IGetJobFiltersResponse filtersResponse = testConnector.JobFilters<Test_GetJobFiltersRequest, Test_GetJobFiltersResponse>(new Test_GetJobFiltersRequest());
-
-            ////Execute - Try perform a dummy search
-            //ISearchJobsRequest request = new Test_SearchJobsRequest { Page = 0, PageSize = 2, FiltersSearch = new List<FiltersSearchRoot> { new FiltersSearchRoot { RootID = "AE-1234", Filters = new List<FiltersSearchElement> { new FiltersSearchElement { ID = "DD-3123" } } } } };
-            //ISearchJobsResponse response = testBusinessLogicConnector.SearchJobs(request);
-
         }
 
     }

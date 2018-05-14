b0ad937dc77b704933e5e76a6e33daaddc22c07b
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-05-14 11:44:43 +1000

    Job Search Results widget initial check-in:only requires files checked-in, no implementation

---
+++ SitefinityWebApp/Mvc/Controllers/JobSearchResultsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+using Telerik.Sitefinity.Web;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "JobSearchResults_MVC", Title = "Job Search Results", SectionName = "Search", CssClass = JobSearchResultsController.WidgetIconCssClass)]
+    public class JobSearchResultsController : Controller
+    {
+        // GET: JobSearchResults
+        public ActionResult Index()
+        {
+            return View("Simple");
+        }
+
+        internal const string WidgetIconCssClass = "sfMvcIcn";
+    }
+}

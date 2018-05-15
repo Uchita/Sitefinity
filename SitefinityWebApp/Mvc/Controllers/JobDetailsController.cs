99d666e85f10350b2fac8c81afd2e3e3c132f579
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-05-15 11:50:40 +1000

    Initial check-in for job details widget

---
+++ SitefinityWebApp/Mvc/Controllers/JobDetailsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "JobDetails_MVC", Title = "Job Details", SectionName = "Search", CssClass = JobDetailsController.WidgetIconCssClass)]
+    public class JobDetailsController : Controller
+    {
+        // GET: JobDetails
+        public ActionResult Index()
+        {
+            return View("Simple");
+        }
+
+        internal const string WidgetIconCssClass = "sfMvcIcn";
+    }
+}

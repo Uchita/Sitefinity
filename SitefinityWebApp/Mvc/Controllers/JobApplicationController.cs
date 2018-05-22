d87f7eb828605661b52e8bbfe66ce77805a12106
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-05-22 17:15:36 +1000

    Job Application widget initial check-in

---
+++ SitefinityWebApp/Mvc/Controllers/JobApplicationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "JobApplication_MVC", Title = "Job Application", SectionName = "Search", CssClass = JobApplicationController.WidgetIconCssClass)]
+    public class JobApplicationController : Controller
+    {
+        // GET: JobApplication
+        public ActionResult Index()
+        {
+            return View("Simple");
+        }
+
+        internal const string WidgetIconCssClass = "sfMvcIcn";
+    }
+}

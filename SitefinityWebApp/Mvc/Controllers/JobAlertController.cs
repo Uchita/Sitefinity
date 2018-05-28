39e3010b1fe383d756c36a0622d0087cad7e1686
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-05-29 09:40:52 +1000

    Job Alert Initial check-in

---
+++ SitefinityWebApp/Mvc/Controllers/JobAlertController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "JobAlert_MVC", Title = "Job Alert", SectionName = "Search", CssClass = JobAlertController.WidgetIconCssClass)]
+    public class JobAlertController : Controller
+    {
+        // GET: JobAlert
+        public ActionResult Index()
+        {
+            return View("Simple");
+        }
+
+        internal const string WidgetIconCssClass = "sfMvcIcn";
+    }
+}

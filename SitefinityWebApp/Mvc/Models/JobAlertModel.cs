2d1af4fa3ef39cec3b424094c45c5288aa658d7d
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-05-31 10:08:24 +1000

    Job Alert Edit view post and get.

--- SitefinityWebApp/Mvc/Models/JobAlertModel.cs
+++ SitefinityWebApp/Mvc/Models/JobAlertModel.cs
@@ -20,4 +20,28 @@ namespace SitefinityWebApp.Mvc.Models
         public string RootId { get; set; }
         public List<string> Values { get; set; }
     }
+
+    public class JobAlertEditViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool EmailAlerts { get; set; }
+        public string Keywords { get; set; }
+        public List<JobAlertEditFilterRootItem> Data { get; set; }
+    }
+
+    public class JobAlertEditFilterRootItem
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public List<JobAlertEditFilterItem> Filters { get; set; }
+    }
+
+    public class JobAlertEditFilterItem
+    {
+        public string ID { get; set; }
+        public string Label { get; set; }
+        public bool Selected { get; set; }
+        public List<JobAlertEditFilterItem> Filters { get; set; }
+    }
 }

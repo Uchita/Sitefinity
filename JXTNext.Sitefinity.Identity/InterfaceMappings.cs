543686201b67713690ea1b6aa1b7d880ba05da63
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-06-13 14:55:40 +1000

    Moved the LoginStatus Extended into Idenity assembly name. In the future if we want to extend any idenity widget we can add here.

---
+++ JXTNext.Sitefinity.Identity/InterfaceMappings.cs
@@ -0,0 +1,26 @@
+﻿using Ninject.Modules;
+//using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.AccountActivation;
+//using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.ChangePassword;
+//using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.LoginForm;
+//using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.LoginStatus;
+//using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.Profile;
+//using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.Registration;
+//using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.UsersList;
+using JXTNext.Sitefinity.Identity.LoginStatusExtended.Mvc.Models;
+
+namespace JXTNext.Sitefinity.Identity
+{
+    /// <summary>
+    /// This class is used to describe the bindings which will be used by the Ninject container when resolving classes
+    /// </summary>
+    public class InterfaceMappings : NinjectModule
+    {
+        /// <summary>
+        /// Loads the module into the kernel.
+        /// </summary>
+        public override void Load()
+        {
+            Bind<ILoginStatusExtendedModel>().To<LoginStatusExtendedModel>();
+        }
+    }
+}

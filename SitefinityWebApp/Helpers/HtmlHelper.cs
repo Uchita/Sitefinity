e3fb6a6a0a9ca1586590b79f2ae250fe208038ca
Himmy <himmy@jxt.com.au>
2018-06-28 15:54:09 +1000

    Added helper for getting image url

---
+++ SitefinityWebApp/Helpers/HtmlHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Sitefinity.Model;
+using Telerik.Sitefinity.Frontend.Mvc.Models;
+
+namespace JXTNext.Sitefinity.Common.Helpers
+{
+    public static class HtmlHelper
+    {
+        public static string GetItemImageUrl(this ItemViewModel dataItem, string targetField, int imageIndex)
+        {
+            if (dataItem.Fields == null || dataItem.Fields.Image == null 
+                || dataItem.Fields.Image.Length == 0 || dataItem.Fields.Image.Length < imageIndex 
+                || dataItem.Fields.Image[imageIndex] == null)
+                return string.Empty;
+
+            var imageDataItem = dataItem.Fields.Image[imageIndex].DataItem;
+
+            return imageDataItem.Url;
+        }
+
+        public static string GetItemThumbnailImageUrl(this ItemViewModel dataItem, string targetField, int imageIndex)
+        {
+            if (dataItem.Fields == null || dataItem.Fields.Image == null || dataItem.Fields.Image[imageIndex] == null)
+                return string.Empty;
+
+            var imageDataItem = dataItem.Fields.Image[imageIndex].DataItem;
+
+            return imageDataItem.ThumbnailUrl;
+        }
+
+    }
+}

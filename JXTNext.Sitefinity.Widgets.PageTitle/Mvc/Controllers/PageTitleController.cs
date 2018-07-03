a91212349bceb6ebb069a3dcbc366ab1acf0e5c3
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-07-04 03:46:17 +1000

    Page Title widget

---
+++ JXTNext.Sitefinity.Widgets.PageTitle/Mvc/Controllers/PageTitleController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
+using Telerik.Sitefinity.Web;
+using Telerik.Sitefinity.Modules.Pages;
+using JXTNext.Sitefinity.Widgets.PageTitle.Mvc.Models;
+
+namespace JXTNext.Sitefinity.Widgets.PageTitle.Mvc.Controllers
+{
+    [EnhanceViewEngines]
+    [ControllerToolboxItem(Name = "PageTitle_MVC", Title = "Page Title", SectionName = "JXTNext.PageTitle", CssClass = PageTitleController.WidgetIconCssClass)]
+    public class PageTitleController : Controller
+    {
+        public ActionResult Index()
+        {
+            string pageTitle = String.Empty;
+            if (this.IsDefaultTitle)
+                pageTitle = SiteMapBase.GetActualCurrentNode().Title;
+            else
+                pageTitle = this.CustomPageTitle;
+
+            var viewModel = new PageTitleViewModel() { PageTitle = pageTitle };
+
+            return View("Simple", viewModel);
+        }
+
+        public string CssClass { get; set; }
+        public string CustomPageTitle { get; set; }
+        public bool IsDefaultTitle { get; set; }
+        internal const string WidgetIconCssClass = "sfMvcIcn";
+    }
+}
---
+++ JXTNext.Sitefinity.Widgets.PageTitle/Mvc/Models/PageTitleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXTNext.Sitefinity.Widgets.PageTitle.Mvc.Models
+{
+    public class PageTitleViewModel
+    {
+        public string PageTitle { get; set; }
+    }
+}

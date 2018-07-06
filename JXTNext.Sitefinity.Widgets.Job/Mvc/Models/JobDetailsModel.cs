c18cf421f4f53a2d9f5744bc2e8231ae1e02f219
Himmy <himmy@jxt.com.au>
2018-07-06 17:46:06 +1000

    revised

---
+++ JXTNext.Sitefinity.Widgets.Job/Mvc/Models/JobDetailsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXTNext.Sitefinity.Widgets.Job.Mvc.Models
+{
+    public class JobDetailsViewModel
+    {
+        public dynamic JobDetails { get; set; }
+        public bool JobApplyAvailable { get; set; }
+    }
+}

e1ebeb053e003339b4546ed958d24f66c4935e3b
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-07-25 16:47:50 +1000

    Merge selected filters in filters widget.

--- JXTNext.Sitefinity.Widgets.Job/Mvc/Controllers/JobFiltersController.cs
+++ JXTNext.Sitefinity.Widgets.Job/Mvc/Controllers/JobFiltersController.cs
@@ -43,18 +43,122 @@ namespace JXTNext.Sitefinity.Widgets.Job.Mvc.Controllers
         }
 
         // GET: JobFilters
-        public ActionResult Index(JobSearchResultsFilterModel filterModel)
+        public ActionResult Index([ModelBinder(typeof(JobSearchResultsFilterBinder))] JobSearchResultsFilterModel filterModel)
         {
             dynamic dynamicFilterResponse = null;
             JXTNext_GetJobFiltersRequest request = new JXTNext_GetJobFiltersRequest();
             IGetJobFiltersResponse filtersResponse = _OConnector.JobFilters<JXTNext_GetJobFiltersRequest, JXTNext_GetJobFiltersResponse>(request);
+
+            List<JobFilterRoot> filtersVMList = null;
             if (filtersResponse != null && filtersResponse.Filters != null
                 && filtersResponse.Filters.Data != null)
+            {
+                filtersVMList = filtersResponse.Filters.Data;
                 dynamicFilterResponse = filtersResponse.Filters.Data as dynamic;
+            }
 
+            var filtersSelected = filterModel.Filters;
+
+            if(filtersSelected != null && filtersSelected.Count > 0)
+                ProcessFilters(filtersSelected, filtersVMList);
+     
             ViewBag.FilterModel = JsonConvert.SerializeObject(filterModel);
+            ViewBag.Keywords = filterModel.Keywords;
+    
+            dynamicFilterResponse = filtersVMList as dynamic;
 
             return View(this.TemplateName, dynamicFilterResponse);
+        }
+
+        static void ProcessFilters(List<JobSearchFilterReceiver> selectedFilters, List<JobFilterRoot> filtersVMList)
+        {
+            if (selectedFilters != null && selectedFilters.Count > 0)
+            {
+                foreach (var rootItem in selectedFilters)
+                {
+                    if (rootItem != null)
+                    {
+                        if (filtersVMList != null && filtersVMList.Count > 0)
+                        {
+                            foreach (var filterVMRootItem in filtersVMList)
+                            {
+                                if (filterVMRootItem.Name == rootItem.rootId)
+                                {
+                                    if (filterVMRootItem.Filters != null && filterVMRootItem.Filters.Count > 0)
+                                    {
+                                        foreach (var filterItem in filterVMRootItem.Filters)
+                                        {
+                                            MergeFilters(filterItem, rootItem.values);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        static bool CheckSubTragets(JobSearchFilterReceiverItem filterRecItem, string Id)
+        {
+            if(filterRecItem != null)
+            {
+                if(filterRecItem.ItemID.Equals(Id, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+                else
+                {
+                    if(filterRecItem.SubTargets != null && filterRecItem.SubTargets.Count > 0)
+                    {
+                        foreach(var item in filterRecItem.SubTargets)
+                        {
+                            if (CheckSubTragets(item, Id))
+                                return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        static void MergeFilters(JobFilter filterItem, List<JobSearchFilterReceiverItem> values)
+        {
+            if(filterItem != null)
+            {
+                if (values != null && values.Count > 0)
+                {
+                    foreach(var item in values)
+                    {
+                        if(item.ItemID.Equals(filterItem.ID, StringComparison.OrdinalIgnoreCase))
+                        {
+                            filterItem.Selected = true;
+                            break;
+                        }
+
+                        if(item.SubTargets != null && item.SubTargets.Count > 0)
+                        {
+                            foreach(var subItem in item.SubTargets)
+                            {
+                                if(CheckSubTragets(item, filterItem.ID))
+                                {
+                                    filterItem.Selected = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    if (filterItem.Filters != null && filterItem.Filters.Count > 0)
+                    {
+                        foreach (var item in filterItem.Filters)
+                        {
+                            MergeFilters(item, values);
+                        }
+                    }
+                }
+            }
         }
 
         protected override void HandleUnknownAction(string actionName)

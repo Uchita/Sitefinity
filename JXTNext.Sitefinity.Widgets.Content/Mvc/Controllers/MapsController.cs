82a8b1c4c33559c54541b9b6b22964c33e0cd8fc
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-08-15 09:54:38 +1000

    Maps widget intial check-in.

---
+++ JXTNext.Sitefinity.Widgets.Content/Mvc/Controllers/MapsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
+using Telerik.Sitefinity.Web;
+using JXTNext.Sitefinity.Widgets.Content.Mvc.Models;
+using JXTNext.Sitefinity.Common.Helpers;
+
+namespace JXTNext.Sitefinity.Widgets.Content.Mvc.Controllers
+{
+    [EnhanceViewEngines]
+    [ControllerToolboxItem(Name = "Maps_MVC", Title = "Maps", SectionName = "JXTNext.Content", CssClass = MapsController.WidgetIconCssClass)]
+    public class MapsController : Controller
+    {
+
+        /// <summary>
+        /// Gets or sets the name of the template that widget will be displayed.
+        /// </summary>
+        /// <value></value>
+        public string TemplateName
+        {
+            get
+            {
+                return this.templateName;
+            }
+
+            set
+            {
+                this.templateName = value;
+            }
+        }
+
+        public ActionResult Index()
+        {
+            var viewModel = new MapsViewModel();
+            viewModel.Address = this.Address;
+            viewModel.GoogleMapsAPIKey = new SiteSettingsHelper().GetCurrentSiteGoogleMapsAPIKey();
+            var fullTemplateName = this.templateNamePrefix + this.TemplateName;
+            ViewBag.CssClass = CssClass;
+            return View(fullTemplateName, viewModel);
+        }
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            this.ActionInvoker.InvokeAction(this.ControllerContext, "Index");
+        }
+
+        public string CssClass { get; set; }
+        public string Address { get; set; }
+        internal const string WidgetIconCssClass = "sfMvcIcn";
+        private string templateName = "Simple";
+        private string templateNamePrefix = "Maps.";
+    }
+}
---
+++ JXTNext.Sitefinity.Widgets.Content/Mvc/Models/MapsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXTNext.Sitefinity.Widgets.Content.Mvc.Models
+{
+    public class MapsViewModel
+    {
+        public string Address { get; set; }
+        public string GoogleMapsAPIKey { get; set; }
+    }
+}

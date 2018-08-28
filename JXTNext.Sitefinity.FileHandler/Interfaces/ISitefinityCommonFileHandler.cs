e62ebb6f4d293817bc37bac958139520970e2ad1
Hashan Siriwardena <hashan@jxt.com.au>
2018-08-28 10:39:35 +1000

    Adding Sitefinity FileHandler to the JXTNEXT Project

---
+++ JXTNext.Sitefinity.FileHandler/Interfaces/ISitefinityCommonFileHandler.cs
@@ -0,0 +1,22 @@
+﻿using JXTNext.Sitefinity.FileHandler.Enums;
+using JXTNext.Sitefinity.FileHandler.Models.Base;
+
+namespace JXTNext.Sitefinity.FileHandler.Interfaces
+{
+    public interface ISitefinityFileHandler
+    {
+        /// <summary>
+        /// Helps select the type of file
+        /// </summary>
+        SitefinityCommonFileHandlerServiceType sitefinityCommonFileHandlerServiceType { get; }
+
+        TReturnType ProcessFileUpload<TReturnType, TParameterType>(TParameterType uploadFileParameters)
+            where TReturnType : SitefinityCommonFileHandlerResponseModelBase
+            where TParameterType : SitefinityCommonFileHandlerRequestModelBase;
+
+        TReturnType ProcessFileDownload<TReturnType, TParameterType>(TParameterType downloadFileParameters)
+            where TReturnType : SitefinityCommonFileHandlerResponseModelBase
+            where TParameterType : SitefinityCommonFileHandlerRequestModelBase;
+
+    }
+}

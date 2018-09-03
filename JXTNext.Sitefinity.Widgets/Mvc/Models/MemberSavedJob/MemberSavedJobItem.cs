b7bd236047a57a02f572ebb2e0307393109b2260
Himmy <himmy@jxt.com.au>
2018-09-03 16:27:24 +1000

    new widget for displaying member saved jobs

---
+++ JXTNext.Sitefinity.Widgets/Mvc/Models/MemberSavedJob/MemberSavedJobItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXTNext.Sitefinity.Widgets.User.Mvc.Models.MemberSavedJob
+{
+    public class MemberSavedJobDisplayItem
+    {
+        public int Id { get; set; }
+        public int JobId { get; set; }
+        public string DisplayTitle { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
---
+++ JXTNext.Sitefinity.Widgets/Widgets_UserModule.cs
@@ -0,0 +1,19 @@
+﻿using JXTNext.Sitefinity.Widgets.User.Mvc.Logics;
+using Ninject.Modules;
+
+namespace JXTNext.Sitefinity.Widgets.Job
+{
+    /// <summary>
+    /// This class is used to describe the bindings which will be used by the Ninject container when resolving classes
+    /// </summary>
+    public class Widgets_UserModule : NinjectModule
+    {
+        /// <summary>
+        /// Loads the module into the kernel.
+        /// </summary>
+        public override void Load()
+        {
+            Bind<MemberSavedJobBC>();
+        }
+    }
+}

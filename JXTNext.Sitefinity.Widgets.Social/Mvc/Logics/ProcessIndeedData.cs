d6946dff0ff3c14f8276fe2cb1e16ee4cf9f33a7
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-09-28 11:55:47 +1000

    Social handler for apply with social media in job application.

---
+++ JXTNext.Sitefinity.Widgets.Social/Mvc/Logics/ProcessIndeedData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXTNext.Sitefinity.Widgets.Social.Mvc.Logics
+{
+    public class ProcessIndeedData : IProcessSocialMediaData
+    {
+        // Constructor
+        public ProcessIndeedData()
+        {
+
+        }
+
+        // Interface method
+        public (string, bool) ProcessData(string data)
+        {
+            return (null, true);
+        }
+    }
+}
---
+++ JXTNext.Sitefinity.Widgets.Social/Mvc/Logics/SocialHandlerLogics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXTNext.Sitefinity.Widgets.Social.Mvc.Logics
+{
+    public class SocialHandlerLogics
+    {
+        IEnumerable<IProcessSocialMediaData> _processSocialMediaData;
+        public SocialHandlerLogics(IEnumerable<IProcessSocialMediaData> processSocialMediaData)
+        {
+            _processSocialMediaData = processSocialMediaData;
+        }
+
+        public string ProcessSocialHandlerData(string data)
+        {
+            foreach(var item in _processSocialMediaData)
+            {
+                (string result, bool isProcessed) = item.ProcessData(data);
+                if(isProcessed)
+                    return result;
+            }
+
+            return null;
+        }
+    }
+}

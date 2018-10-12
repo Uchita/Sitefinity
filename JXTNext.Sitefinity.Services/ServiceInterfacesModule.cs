639a81dde6b322b5eae5b7a631fcbe409a456a23
DESKTOP-9IO2A6H\Sathish <sathish@jxt.com.au>
2018-10-12 15:10:36 +1100

    JXTNext services

---
+++ JXTNext.Sitefinity.Services/ServiceInterfacesModule.cs
@@ -0,0 +1,24 @@
+﻿using JXTNext.Sitefinity.Services.Intefaces;
+using JXTNext.Sitefinity.Services.Services;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXTNext.Sitefinity.Services.Services
+{
+    public class ServiceInterfacesModule : NinjectModule
+    {
+        public ServiceInterfacesModule()
+        {
+
+        }
+
+        public override void Load()
+        {
+             Bind<IJobApplicationService>().To<JXTNextJobApplicationService>();
+        }
+    }
+}

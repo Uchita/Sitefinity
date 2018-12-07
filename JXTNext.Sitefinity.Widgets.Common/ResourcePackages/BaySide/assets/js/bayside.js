c557370445e695a58abcea1ab722444687c20dc9
gaurav0503 <gaurav.budhiraja@idslogic.com>
2018-12-07 17:48:00 +0530

    trackduck tickets changes regarding bayside and resolved the navigation related issues.

--- JXTNext.Sitefinity.Widgets.Common/ResourcePackages/BaySide/assets/js/bayside.js
+++ JXTNext.Sitefinity.Widgets.Common/ResourcePackages/BaySide/assets/js/bayside.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         new WOW().init();
     }
     $("#featuredInsights,#internalJobs").owlCarousel({
-        margin: 30,
+        margin: 32,
         nav: true,
         dots: false,
         navText: ['<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.61 7.41L14.2 6l-6 6 6 6 1.41-1.41L11.03 12l4.58-4.59z"/></svg>',
@@ -29,7 +29,7 @@ $(document).ready(function () {
             '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10.02 6L8.61 7.41 13.19 12l-4.58 4.59L10.02 18l6-6-6-6z"/></svg>'
         ],
         items: 1,
-        dots: true
+        dots: true,
     });
 
     /**Leaders Page accordion */
@@ -68,6 +68,45 @@ $(document).ready(function () {
         window.location.href = $(this).attr("href");
     });
 
+    /*
+     * ------------------------------------
+     * ------- AK Counter
+     * ------------------------------------
+     */
+    (function ($) {
+        "use strict";
+        var winHeight = $(window).height();
+        $(window).on("scroll", function () {
+            var winScroll = $(window).scrollTop();
+            var $counters = $(".item-count");
+            $counters.each(function (idx, el) {
+                var $counter = $(el);
+                var elPos = $counter.closest(".section-graphics").offset().top;
+                var dataSt = $counter.attr("data-st");
+                var matchPos = (elPos - (winHeight - 100));
+                var text = $counter.attr("data-text");
+                if (winScroll > matchPos) {
+                    if (dataSt == '1') {
+                        $counter.attr("data-st", '0');
+                        $counter.prop('Counter', 0).animate({
+                            Counter: text
+                        }, {
+                                duration: 2000,
+                                easing: 'swing',
+                                step: function (now) {
+                                    $counter.text(parseInt(now));
+                                }
+                            });
+                    }
+                } else {
+                    $counter.attr("data-st", '1');
+                    $counter.text("0");
+                }
+            });
 
+        });
+
+    }(jQuery));
 
 });
+
--- JXTNext.Sitefinity.Widgets.Job/Mvc/Controllers/JobSearchResultsController.cs
+++ JXTNext.Sitefinity.Widgets.Job/Mvc/Controllers/JobSearchResultsController.cs
@@ -208,7 +208,7 @@ namespace JXTNext.Sitefinity.Widgets.Job.Mvc.Controllers
                 dynamicJobResultsList = response as dynamic;
             }
 
-            PartialViewResult jobResultsPartialVR = PartialView("_JobSearchResults", dynamicJobResultsList);
+            PartialViewResult jobResultsPartialVR = PartialView("_JobSearchResultsBayside", dynamicJobResultsList);
             JobFiltersController jobFiltersController = new JobFiltersController(_bConnectorsList, _oConnectorsList);
             ActionResult filtersActionResult = jobFiltersController.Index(filterModel, SiteMapBase.GetActualCurrentNode().Title, (dynamicJobResultsList != null) ? dynamicJobResultsList.SearchResultsFilters : null);
 

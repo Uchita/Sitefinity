2d6bb53c77394d310ec2e396b2b2865a8d1e99bf
Aditya Maurya <aditya.maurya@idslogic.com>
2018-12-14 16:14:02 +0530

    mobile menu style

--- JXTNext.Sitefinity.Widgets.Common/ResourcePackages/PCN/assets/dist/js/custom.js
+++ JXTNext.Sitefinity.Widgets.Common/ResourcePackages/PCN/assets/dist/js/custom.js
@@ -1,39 +1,50 @@
 $(document).ready(function () {
-    
+
     /* logo_slider */
     $('.logo_slider').owlCarousel({
-        center:true,
-        loop:true,
-        margin:40,
-        autoWidth:true,
-        autoplay:true,
-        autoplayTimeout:2000,
-        nav:false,
-        responsive:{
-            0:{
-                items:2
+        center: true,
+        loop: true,
+        margin: 40,
+        autoWidth: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        nav: false,
+        responsive: {
+            0: {
+                items: 2
             },
-            768:{
-                items:4
+            768: {
+                items: 4
             },
-            1200:{
-                items:6
+            1200: {
+                items: 6
             }
         }
     });
-    
+
     /* mobile menu */
-    $('.menu_toogle').click(function(){
+    $('.menu_toogle').click(function () {
         $('.header_right').addClass('open');
         $('.header').addClass('menu_open');
+        $('.header .main_menu ul li span').addClass('fa-angle-right');
     });
-    
-    $('.menu_close').click(function(){
+
+    $('.menu_close').click(function () {
         $('.header_right').removeClass('open');
         $('.header').removeClass('menu_open');
+        $('.header .main_menu ul li span').removeClass('fa-angle-right');
     });
-    
+
+    $('.header .main_menu ul li .mobile-arrow').click(function () {
+        $(this).next('ul').slideToggle();
+        $(this).toggleClass('fa-angle-right');
+        $(this).toggleClass('fa-angle-down');
+    });
+
 });
+
+
+
 
 
 

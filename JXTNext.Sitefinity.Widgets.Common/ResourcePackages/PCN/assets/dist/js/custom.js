f2aad77e63b13e17e6aee07e12705588afdac184
vipul <vipul.singh@idslogic.com>
2019-01-08 20:32:57 +0530

    updates on track duck points

--- JXTNext.Sitefinity.Widgets.Common/ResourcePackages/PCN/assets/dist/js/custom.js
+++ JXTNext.Sitefinity.Widgets.Common/ResourcePackages/PCN/assets/dist/js/custom.js
@@ -1,5 +1,18 @@
-$(document).ready(function () {
 
+
+$(document).ready(function () {
+    var x = getCookie('concent');
+    if (x != '') {
+        $('.cookie-banner-container').remove();
+    }
+
+    // Video Auto Play
+    var video = $(".bg-video video")[0];
+    if (video) {
+        video.autoplay = true;
+        video.loop = true;
+        video.muted = true;
+    }
     /* logo_slider */
     $('.logo_slider').owlCarousel({
         center: true,
@@ -74,6 +87,8 @@ $(document).ready(function () {
 
 
 
+
+
 });
 
 
@@ -134,6 +149,29 @@ $(window).on("load", function () {
     equalHeightAp('.equalHeight');
 });
 
+
+$('.accept-link').click(function () {
+    $('.cookie-banner-container').remove();
+    var x = getCookie('concent');
+    if (x == '' || x == null) {
+        document.cookie = "concent=true";
+    }
+});
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
 
 
 

e8ff452c6618e7a878add1ea5e231625d8b0afad
vipul <vipul.singh@idslogic.com>
2019-01-21 15:28:57 +0530

    event and design changes

--- JXTNext.Sitefinity.Widgets.Common/ResourcePackages/PCN/assets/dist/js/custom.js
+++ JXTNext.Sitefinity.Widgets.Common/ResourcePackages/PCN/assets/dist/js/custom.js
@@ -59,6 +59,30 @@ $(document).ready(function () {
         items: 1
     });
 
+    /* job-slider */
+    $('#job-slider').owlCarousel({
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
+
     /* mobile menu */
     $('.menu_toogle').click(function () {
         $('.header_right').addClass('open');

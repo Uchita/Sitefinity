2144362c0a8700d545017eaacb63d77aae6558d5
Suraj <suraj@jxt.com.au>
2019-04-01 18:57:05 +1100

    dropdown tree list updated to expand without selecting child node + dashboard tables pagination

--- JXTNext.Sitefinity.Widgets.Common/ResourcePackages/PCN/assets/dist/js/custom.js
+++ JXTNext.Sitefinity.Widgets.Common/ResourcePackages/PCN/assets/dist/js/custom.js
@@ -151,6 +151,8 @@ $(document).ready(function () {
         }
     });
 
+
+
     /* mobile menu */
     $('.menu_toogle').click(function () {
         $('.header_right').addClass('open');
@@ -230,6 +232,37 @@ $(document).ready(function () {
         }
     });
 
+
+    //using dataTable plugin for pagination in table
+    if ($('table.datatable').length) {
+        $("table.datatable").each(function () {
+            var tbl = $(this);
+            var dateColIndex = tbl.find('th.date-col').index();
+            var actColIndex = tbl.find('th.act-col').index();
+            if (dateColIndex < 0) {
+                dateColIndex = 0;
+            }
+            tbl.slimtable({
+                itemsPerPage: 5,
+                ippList: [5, 10, 20],
+                sortList: [dateColIndex],
+                colSettings: [{
+                    sortDir: "desc",
+                    colNumber: dateColIndex
+                },
+                {
+                    enableSort: false,
+                    colNumber: actColIndex
+                }
+                ],
+            });
+
+            if (tbl.parent().find('.slimtable-page-btn').length < 2) {
+                tbl.parent().find('.slimtable-paging-div').hide();
+            }
+        });
+
+    }
 });
 
 

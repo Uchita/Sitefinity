667584ce15af8e424b938a539333147c59dcecab
Lee_rbt <leela.hari@raybiztech.com>
2019-04-01 09:12:53 +0530

    Qa Changes

--- JXTNext.Sitefinity.Connector/Base/ConnectorBase.cs
+++ JXTNext.Sitefinity.Connector/Base/ConnectorBase.cs
@@ -50,7 +50,8 @@ namespace JXTNext.Sitefinity.Connector
         private void ProcessHeaderValuesForSession(IRequestSession session)
         {
             _headerValues = new Dictionary<string, string>();
-            _headerValues.Add(API_HTTP_HEADER_DOMAIN_KEY, session.Domain);
+           // _headerValues.Add(API_HTTP_HEADER_DOMAIN_KEY, session.Domain);
+            _headerValues.Add(API_HTTP_HEADER_DOMAIN_KEY, "13.54.108.200");
             if ( session.UserEmail != null)
                 _headerValues.Add(API_HTTP_HEADER_USER_KEY, session.UserEmail);
         }

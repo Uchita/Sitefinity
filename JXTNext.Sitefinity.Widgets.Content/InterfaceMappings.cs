34daffa1ef153bfa3698175c3cc0c5b3ee321118
Seth Cleaver <seth@cleaver.id.au>
2019-04-23 03:10:46 +1000

    Add page banner widget

---
+++ JXTNext.Sitefinity.Widgets.Content/InterfaceMappings.cs
@@ -0,0 +1,19 @@
+﻿using JXTNext.Sitefinity.Widgets.Content.Mvc.Models.PageBanner;
+using Ninject.Modules;
+
+namespace JXTNext.Sitefinity.Widgets.Content
+{
+    /// <summary>
+    /// This class is used to describe the bindings which will be used by the Ninject container when resolving classes
+    /// </summary>
+    public class InterfaceMappings : NinjectModule
+    {
+        /// <summary>
+        /// Loads the module into the kernel.
+        /// </summary>
+        public override void Load()
+        {
+            Bind<IPageBannerModel>().To<PageBannerModel>();
+        }
+    }
+}
---
+++ JXTNext.Sitefinity.Widgets.Content/Mvc/Models/PageBanner/PageBannerViewModel.cs
@@ -0,0 +1,42 @@
+﻿using SfImage = Telerik.Sitefinity.Libraries.Model.Image;
+
+namespace JXTNext.Sitefinity.Widgets.Content.Mvc.Models.PageBanner
+{
+    public class PageBannerViewModel
+    {
+        /// <summary>
+        /// Gets or sets the heading.
+        /// </summary>
+        public string Heading { get; set; }
+
+        /// <summary>
+        /// Gets or sets the HTML description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the selected image.
+        /// </summary>
+        public SfImage Image { get; set; }
+
+        /// <summary>
+        /// Gets or sets the selected size image URL.
+        /// </summary>
+        public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the current page is the homepage.
+        /// </summary>
+        public bool IsHomepage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the css classes.
+        /// </summary>
+        public string CssClass { get; set; }
+
+        /// <summary>
+        /// Gets or sets the widget's css class.
+        /// </summary>
+        public string DefaultCssClass { get; set; }
+    }
+}

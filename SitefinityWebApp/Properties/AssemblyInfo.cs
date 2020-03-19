6a8260b44b84d4ca4e63182e7c7d39dcb4006821
Amir <amir@jxt.com.au>
2020-03-20 09:36:03 +1100

    Sitefinity Version 12.2 Upgrade (#255)
    * Upgrade 12.2
    
    * added icon
    
    * 12.2 upgrade

--- SitefinityWebApp/Properties/AssemblyInfo.cs
+++ SitefinityWebApp/Properties/AssemblyInfo.cs
@@ -47,6 +47,8 @@ using System.Resources;
 
 
 
+
+
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ControllerContainer]
 
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ResourcePackage]
